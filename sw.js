886966f040d75d912007989f747b46571d213cfd
felix.zheng91 <felix.zheng91@outlook.com>
2026-09-03 10:31:16 +0800

    feat: add more templates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="20260901164000";const CACHE="fitlog-"+VERSION;const ASSETS=["index.html","./","manifest.webmanifest","icon.svg"];
+const VERSION="20260903103000";const CACHE="fitlog-"+VERSION;const ASSETS=["index.html","./","manifest.webmanifest","icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting();});
